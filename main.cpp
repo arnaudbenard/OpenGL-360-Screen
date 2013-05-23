572701b7deb0e1f9472459965ec5f87d6dcdb362
Arnaud Benard <arnaudbenard13@gmail.com>
2013-05-22 19:35:33 -0700

    .gitignore is now working

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 // Globales
 double dim=2; /* dimension of orthogonal box */
 float xMin=-1,yMin=-1,xMax=1,yMax=1;
-static int windowSize = 100,size,el_size;
+static int windowSize = 20,size,el_size;
 GLuint ID;
 double yaw=0,pitch=0,roll=0, /* If z pointing at me:
                                 yaw: z-axis rotation
@@ -15,15 +15,22 @@ strafe=0,jump=0,dir=0; /* strafe: side to side movement.
                           jump: up down  movement
                           dir: is the direction you're pointing at*/
 int first=1;
-void rotate(void){
-
-
+void checkGLError( const char hdr[] )
+{
+  int err = glGetError();
+  if( err )
+  {
+    fprintf(stderr, "ERROR %s: %s\n", hdr, gluErrorString(err));
+    exit(1);
+  }
 }
 void savePixelValuesToPNG(void){
 
   cout << "Save to PNG" << endl;
   float* pixels = (float*)malloc(windowSize*windowSize*sizeof(float));
-  glReadPixels(0, 0, windowSize, windowSize, GL_DEPTH_COMPONENT, GL_FLOAT, pixels);
+  glReadBuffer( GL_COLOR_ATTACHMENT0 );
+
+  glReadPixels(0, 0, windowSize, windowSize, GL_RED, GL_FLOAT, pixels);
 
   png::image< png::rgb_pixel > image(windowSize, windowSize);
 
@@ -33,11 +40,10 @@ void savePixelValuesToPNG(void){
   {
       for (size_t x = 0; x < image.get_width(); ++x)
       {
-        pixelValue=(pixels[(x+1) * (y+1)-1])*255;
-        
+        pixelValue=(pixels[(x+1) * (y+1)-1]);
+       // cout << pixels[x] <<endl;
         image[y][x] = png::rgb_pixel(pixelValue,pixelValue,pixelValue);
       }
-      cout <<endl;
    }
 
    image.write("rgb.png");
@@ -53,8 +59,8 @@ void initVBO(void){
   //Loading data from the text files and applying the Delaunay algorithm
   triangulateio fullData=triangulateXYZ();
   vector<float> fullData_vector(fullData.numberoftriangles*3*6); //3 points with 6 coordinates (position and color)
-
-  size=fullData_vector.size(); //Number of points
+  size=18;
+  //size=fullData_vector.size(); //Number of points
   el_size=6*sizeof(float); //Size of one element (6 points)
 
   cout << "Number of input points " << NBPNTS<< endl;
@@ -70,7 +76,28 @@ void initVBO(void){
   double  zMin=fullData.pointattributelist[NBPNTS+4],
   zMax=fullData.pointattributelist[NBPNTS+5];
 
-  for(int i_tri = 0; i_tri < fullData.numberoftriangles; i_tri++) {
+  fullData_vector[0]=0;
+  fullData_vector[1]=1;
+  fullData_vector[2]=0;
+  fullData_vector[3]=1.0;
+  fullData_vector[4]=0.0;
+  fullData_vector[5]=0.0;
+
+  fullData_vector[6]=1;
+  fullData_vector[7]=0;
+  fullData_vector[8]=0;
+  fullData_vector[9]=0;
+  fullData_vector[10]=1;
+  fullData_vector[11]=0;
+
+  fullData_vector[12]=0;
+  fullData_vector[13]=0;
+  fullData_vector[14]=0;
+  fullData_vector[15]=0;
+  fullData_vector[16]=0;
+  fullData_vector[17]=1;
+
+  /*for(int i_tri = 0; i_tri < fullData.numberoftriangles; i_tri++) {
 
     for (int i_point = 0; i_point < 3; i_point++) {
 
@@ -86,7 +113,7 @@ void initVBO(void){
       fullData_vector[point+4]=z_val;
       fullData_vector[point+5]=z_val;
     }
-  }
+  }*/
   
   GLenum err = glewInit();
 
@@ -139,14 +166,15 @@ void display(void)
 
   glPopMatrix ();
   glFlush();
-
+  
+  savePixelValuesToPNG();
 }
 
 /*
  *  GLUT calls this routine when the window is resized
  */
 void reshape(int width,int height)
-{
+{ cout << "reshape"<<endl;
   double w2h = (height>0) ? (double)width/height:1;
   glViewport(0,0,width,height);
   glMatrixMode(GL_PROJECTION);
@@ -155,12 +183,7 @@ void reshape(int width,int height)
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
 }
-void test(){
-    if(first){
-    savePixelValuesToPNG();
-    first=0;
-  }
-}
+
 /*
  *  Start up GLUT and tell it what to do
  */
@@ -173,8 +196,8 @@ int main(int argc,char* argv[])
   glutCreateWindow("OpenGL");
   initVBO();
   glutDisplayFunc(display);
-  glutReshapeFunc(reshape);
-  glutIdleFunc(test);
+  checkGLError("shoop da snoop");
+  //glutReshapeFunc(reshape);
   glutMainLoop();
 
   return 0;
